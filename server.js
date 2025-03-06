8fd85a4ec9e5bd348e5932a5a940cb095e3b7795
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2025-03-06 08:59:54 -0600

    finished exercise

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ Our Setup -
 we are going to send requests to another API
 so we need a bit more than usual!
 =======================================================*/
-const bodyParser = require('body-parser');
-const express = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 const app = express();
 
-const request = require('request');
-const mongoose = require('mongoose');
-const Book = require('./models/BookModel');
-const Person = require('./models/PersonModel');
+const request = require("request");
+const mongoose = require("mongoose");
+const Book = require("./models/BookModel");
+const Person = require("./models/PersonModel");
 
-mongoose.connect('mongodb://localhost/mongoose-exercises');
+mongoose.connect("mongodb://127.0.0.1:27017/mongoose-exercises");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -21,55 +21,55 @@ app.use(bodyParser.json());
 Create books Collection
 =======================================================*/
 const isbns = [
-	9780156012195, 9780743273565, 9780435905484, 9780140275360, 9780756404741,
-	9780756407919, 9780140177398, 9780316769488, 9780062225672, 9780143130154,
-	9780307455925, 9781501143519,
+  9780156012195, 9780743273565, 9780435905484, 9780140275360, 9780756404741,
+  9780756407919, 9780140177398, 9780316769488, 9780062225672, 9780143130154,
+  9780307455925, 9781501143519,
 ];
 
-const url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:';
+const url = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
 
 const loadFromAPI = (apiURL) => {
-	request(apiURL, function (error, response, body) {
-		const result = JSON.parse(body);
+  request(apiURL, function (error, response, body) {
+    const result = JSON.parse(body);
 
-		if (result.totalItems && !error && response.statusCode == 200) {
-			const resBook = JSON.parse(body).items[0].volumeInfo;
+    if (result.totalItems && !error && response.statusCode == 200) {
+      const resBook = JSON.parse(body).items[0].volumeInfo;
 
-			const book = new Book({
-				title: resBook.title,
-				author: resBook.authors ? resBook.authors[0] : null,
-				pages: resBook.pageCount,
-				genres: resBook.categories || ['Other'],
-				rating: resBook.averageRating || 5,
-			});
+      const book = new Book({
+        title: resBook.title,
+        author: resBook.authors ? resBook.authors[0] : null,
+        pages: resBook.pageCount,
+        genres: resBook.categories || ["Other"],
+        rating: resBook.averageRating || 5,
+      });
 
-			//Only save if the book doesn't exist yet
-			Book.findOne({ title: book.title }).then((foundBook) => {
-				if (!foundBook) {
-					book.save();
-				}
-			});
-		}
-	});
+      //Only save if the book doesn't exist yet
+      Book.findOne({ title: book.title }).then((foundBook) => {
+        if (!foundBook) {
+          book.save();
+        }
+      });
+    }
+  });
 };
 
 isbns.forEach((i) => {
-	const apiURL = url + i;
-	/*=====================================================
+  const apiURL = url + i;
+  /*=====================================================
   the first time you run your code, uncomment the function below.
   for subsequent runs, re-comment it so that it runs only once!
   that said, there is a fail-safe to avoid duplicates below
   =======================================================*/
-	loadFromAPI(apiURL);
+  // loadFromAPI(apiURL);
 });
 
 /*=====================================================
 Create People Collection
 =======================================================*/
-const colors = ['brown', 'black', 'red', 'yellow', 'green', 'grey'];
+const colors = ["brown", "black", "red", "yellow", "green", "grey"];
 
 const getColor = () => {
-	return colors[Math.floor(Math.random() * colors.length)];
+  return colors[Math.floor(Math.random() * colors.length)];
 };
 
 const getWeight = () => getRandIntBetween(50, 120);
@@ -81,20 +81,20 @@ const getSalary = () => getRandIntBetween(20000, 50000);
 const getNumKids = () => Math.floor(Math.random() * 3);
 
 const getRandIntBetween = (min, max) => {
-	return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 const getKids = (numKids) => {
-	let kids = [];
-	for (let i = 0; i < numKids; i++) {
-		kids.push({
-			hair: getColor(),
-			eyes: getColor(),
-			weight: getWeight(),
-			height: getHeight(),
-		});
-	}
-	return kids;
+  let kids = [];
+  for (let i = 0; i < numKids; i++) {
+    kids.push({
+      hair: getColor(),
+      eyes: getColor(),
+      weight: getWeight(),
+      height: getHeight(),
+    });
+  }
+  return kids;
 };
 
 /*=====================================================
@@ -109,34 +109,34 @@ could you write it differently?
 // the below two loops could be changed to a simple:
 // for (var i = count; i < 100; i++) {}
 Person.countDocuments({})
-	.then((count) => {
-		if (count < 100) {
-			let promises = [];
-			for (let i = 0; i < 100 - count; i++) {
-				let numKids = getNumKids();
-				let p = new Person({
-					hair: getColor(),
-					eyes: getColor(),
-					weight: getWeight(),
-					height: getHeight(),
-					salary: getSalary(),
-					numKids: numKids,
-					kids: getKids(numKids),
-				});
-				promises.push(p.save());
-			}
-			return Promise.all(promises);
-		}
-	})
-	.then(() => console.log('All people saved.'))
-	.catch((err) => console.error('Error during saving people:', err));
+  .then((count) => {
+    if (count < 100) {
+      let promises = [];
+      for (let i = 0; i < 100 - count; i++) {
+        let numKids = getNumKids();
+        let p = new Person({
+          hair: getColor(),
+          eyes: getColor(),
+          weight: getWeight(),
+          height: getHeight(),
+          salary: getSalary(),
+          numKids: numKids,
+          kids: getKids(numKids),
+        });
+        promises.push(p.save());
+      }
+      return Promise.all(promises);
+    }
+  })
+  .then(() => console.log("All people saved."))
+  .catch((err) => console.error("Error during saving people:", err));
 
 /*=====================================================
 Start the server:
 =======================================================*/
 
 app.listen(3000, () => {
-	console.log('Server up and running on port 3000');
+  console.log("Server up and running on port 3000");
 });
 
 /*=====================================================
@@ -147,19 +147,46 @@ and your server is running do the following:
 /*Books
 ----------------------*/
 //1. Find books with fewer than 500 but more than 200 pages
-
+Book.find({ pages: { $gt: 200, $lt: 500 } })
+  .then((books) => console.log("Less than 500, more than 200 pages", books))
+  .catch((err) => console.log("Error:", err));
 //2. Find books whose rating is less than 5, and sort by the author's name
-
+Book.find({ rating: { $lt: 5 } })
+  .sort({ author: 1 })
+  .then((books) =>
+    console.log("Books with rating < 5, sorted by author name", books)
+  )
+  .catch((err) => console.log("Error:", err));
 //3. Find all the Fiction books, skip the first 2, and display only 3 of them
-
+Book.find({ genres: { $regex: /Fiction/, $options: "i" } })
+  .skip(2)
+  .limit(3)
+  .then((books) => console.log("Fiction Books:", books))
+  .catch((err) => console.log("Error:", err));
 /*People
 ----------------------*/
 //1. Find all the people who are tall (>180) AND rich (>30000)
-
+Person.find({ height: { $gt: 180 }, salary: { $gt: 30000 } })
+  .then((people) => console.log("Tall and Rich!", people))
+  .catch((err) => console.log("Error:", err));
 //2. Find all the people who are tall (>180) OR rich (>30000)
-
+Person.find({ $or: [{ height: { $gt: 180 } }, { salary: { $gt: 30000 } }] })
+  .then((people) => console.log("Tall OR Rich:", people))
+  .catch((err) => console.log("Error:", err));
 //3. Find all the people who have grey hair or eyes, and who's weight (<70)
-
+Person.find({ $or: [{ hair: "grey" }, { eyes: "grey" }], weight: { $lt: 70 } })
+  .then((people) =>
+    console.log("Grey hair or eyes, and weigh under 70:", people)
+  )
+  .catch((err) => console.log("Error:", err));
 //4. Find people who have at least 1 kid with grey hair
-
+Person.find({ kids: { $elemMatch: { hair: "grey" } } })
+  .then((people) => console.log("People whose kids have grey hair:", people))
+  .catch((err) => console.log("Error:", err));
 //5. Find all the people who have at least one kid who's weight is >100 and themselves' weight is >100
+Person.find({
+  kids: { $elemMatch: { weight: { $gt: 100 } } },
+  weight: { $gt: 100 },
+})
+  .then((people) => console.log("Fat person, fat kid:", people))
+  .catch((err) => console.log("Error:", err));
